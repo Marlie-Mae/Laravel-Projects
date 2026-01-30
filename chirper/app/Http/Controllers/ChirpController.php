1963f396b9ef10aa55bee665435e959e2aa74102
MarlieMae <awayanmarliemae@gmail.com>
2026-01-30 11:28:14 +0800

    Today's Updates and Learnings

--- chirper/app/Http/Controllers/ChirpController.php
+++ chirper/app/Http/Controllers/ChirpController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class ChirpController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     */
     public function index()
     {
         $chirps = Chirp::with('user')
@@ -15,5 +18,77 @@ class ChirpController extends Controller
             ->get();
 
         return view('home', ['chirps' => $chirps]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    
+   
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ], [
+            'message.required' => 'Please write something to chirp!',
+            'message.max' => 'Chirps must be 255 characters or less.',
+        ]);
+
+        \App\Models\Chirp::create([
+            'message' => $validated['message'],
+            'user_id' => null,
+        ]);
+
+        return redirect('/')->with('success', 'Your chirp has been posted!');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+        
+    public function edit(Chirp $chirp)
+    {
+        // We'll add authorization in lesson 11
+        return view('chirps.edit', compact('chirp'));
+    }
+
+    public function update(Request $request, Chirp $chirp)
+    {
+         if ($request->user()->cannot('update', $chirp)) {
+        abort(403);
+        
+        }
+        // Validate
+        $validated = $request->validate([
+            'message' => 'required|string|max:255',
+        ]);
+
+        // Update
+        $chirp->update($validated);
+
+        return redirect('/')->with('success', 'Chirp updated!');
+    }
+
+    public function destroy(Chirp $chirp)
+    {
+        $chirp->delete();
+
+        return redirect('/')->with('success', 'Chirp deleted!');
     }
 }
--- chirper/resources/views/components/layout.blade.php
+++ chirper/resources/views/components/layout.blade.php
@@ -23,6 +23,23 @@
         </div>
     </nav>
 
+    <!-- Success Toast -->
+    @if (session('success'))
+        <div class="toast toast-top toast-center">
+            <div class="alert alert-success animate-fade-out">
+                <svg xmlns="http://www.w3.org/2000/svg" 
+                    class="h-6 w-6 shrink-0" 
+                    fill="none" 
+                    viewBox="0 0 24 24" 
+                    stroke="black" 
+                    stroke-width="3">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" />
+                </svg>
+                <span>{{ session('success') }}</span>
+            </div>
+        </div>
+    @endif
+
     <!--from views/home.blade.php-->
     <main class="flex-1 container mx-auto px-4 py-8">
         {{ $slot }}
